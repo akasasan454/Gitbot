a770cbadcf75aed07c4f79d55d767dd75be80524
akasasan454 <go_aquarion.0454@icloud.com>
2018-05-09 23:47:35 +0900

    2

--- app/controllers/chatwork_controller.rb
+++ app/controllers/chatwork_controller.rb
@@ -1,9 +1,11 @@
 class ChatworkController < ApplicationController
-	def send
+	def create_task
         ChatWork.api_key = "4fd3ff0947b7bcf450adcff1310fe618"
         unix_time_limit = Time.parse(Date.today.to_s).to_i
-        room_id         = "60381054"
-        to_ids          = "akasasan454"
+        room_id         = 60381054
+        to_ids          = "2119717"
+
+        #puts ChatWork::Me.get
 
         ChatWork::Task.create(
         room_id: room_id,

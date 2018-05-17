527e9b4753e6ed7e546045d0a04e944027eb42fa
akasasan454 <go_aquarion.0454@icloud.com>
2018-05-17 23:00:10 +0900

    7

--- app/controllers/chatwork_controller.rb
+++ app/controllers/chatwork_controller.rb
@@ -1,16 +1,18 @@
 class ChatworkController < ApplicationController
 
 	def create_task
+
+		url = params[:push][:url]
+
+
         ChatWork.api_key = "4fd3ff0947b7bcf450adcff1310fe618"
         unix_time_limit = Time.parse(Date.today.to_s).to_i
         room_id         = 60381054
         to_ids          = "2119717"
 
-        #puts ChatWork::Me.get
-
         ChatWork::Task.create(
         room_id: room_id,
-        body:    "【自動投稿】出勤しなさい",
+        body:    "【自動投稿】出勤しなさい\n#{url}",
         to_ids:  to_ids,
         limit:   unix_time_limit
         )
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'chatwork/task' => 'chatwork#create_task'
+
+  get 'chatwork/task' => 'chatwork#create_task'
 end

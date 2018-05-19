a442971ba5173b30a402b1a50b819cf1da713b6e
akasasan454 <go_aquarion.0454@icloud.com>
2018-05-19 22:33:54 +0900

    22

--- app/controllers/chatwork_controller.rb
+++ app/controllers/chatwork_controller.rb
@@ -3,22 +3,21 @@ class ChatworkController < ApplicationController
 	def create_task
 		if request.body.read.present?
             json_request = JSON.parse(request.body.read)
-        end
         
-        ChatWork.api_key = "4fd3ff0947b7bcf450adcff1310fe618"
-        unix_time_limit = Time.parse(Date.tomorrow.to_s).to_i
-        room_id         = 60381054
-        to_ids          = "2119717"
-        # 2894903
-        body            = "【Pull request自動通知】お手すきで対応お願い致します。\n"
-        body           += "  sender：#{json_request.dig("sender","login")}\n"
-        body           += "  #{json_request.dig("pull_request","html_url")}"
+            ChatWork.api_key = "4fd3ff0947b7bcf450adcff1310fe618"
+            unix_time_limit = Time.parse(Date.tomorrow.to_s).to_i
+            room_id         = 60381054
+            to_ids          = "2119717" # 2894903
+            body            = "【Pull request自動通知】お手すきで対応お願い致します。\n"
+            body           += "  sender：#{json_request.dig("sender","login")}\n"
+            body           += "  #{json_request.dig("pull_request","html_url")}"
 
-        ChatWork::Task.create(
-        room_id: room_id,
-        body:    body,
-        to_ids:  to_ids,
-        limit:   unix_time_limit
-        )
+            ChatWork::Task.create(
+            room_id: room_id,
+            body:    body,
+            to_ids:  to_ids,
+            limit:   unix_time_limit
+            )
+        end
 	end
 end
